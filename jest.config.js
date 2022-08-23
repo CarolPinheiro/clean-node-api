3b25b54f0495d13b242ca4e164919e39139b57c3
Carol Pinheiro <caroline@bossabox.com>
2022-08-23 18:25:21 -0300

    chore: fixed jest config coverage files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/**/protocols/**',
+    '!<rootDir>/**/test/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
