9cea8929d484ae6ac4fa051c20fbdc85bcf40c5c
Carol Pinheiro <caroline@bossabox.com>
2022-12-12 13:49:54 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+export default class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+  constructor(controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.controller.handle(httpRequest)
+    return null
+  }
+}
