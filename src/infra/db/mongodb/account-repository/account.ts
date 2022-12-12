5cb746e1aa3e16b696b53358797b5b6938c5ad0b
Carol Pinheiro <caroline@bossabox.com>
2022-12-12 11:09:14 -0300

    feat: ensure mongodb reconnect if connection is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,12 +4,10 @@ import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
-    const result = await accountCollection
-      .insertOne(accountData)
-    const accountById = await accountCollection
-      .findOne({ _id: result.insertedId })
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const accountById = await accountCollection.findOne({ _id: result.insertedId })
 
     return MongoHelper.map(accountById)
   }
